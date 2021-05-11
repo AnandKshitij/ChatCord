4acaed7fe5420954491efd26c5fd227ea56e031e
kshitijanand <kshitijanand0512@gmail.com>
2021-05-11 19:28:48 +0530

    message communication enabled

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,41 @@
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+
 const socket = io();
 
-socket.on('message', message =>{
-    console.log(message);
+// message from server
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
+
+  // scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
+
+chatForm.addEventListener("submit", (e) => {
+  // when we submit a form it automatically submits to a file
+  // we want to stop this default behaviour from happening
+  e.preventDefault();
+
+  // we need to get the text input
+  // we will be getting that with the element id which is msg
+  const msg = e.target.elements.msg.value;
+
+  // emitting the message to the server
+  socket.emit("chatMessage", msg);
+
+  // clear the input and focus on the empty message typing box
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
+
+// output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
--- server.js
+++ server.js
@@ -12,9 +12,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // run when the client connects
 io.on('connection', socket =>{
-    console.log('New Web Socket Connection');
-
+    // welcome current user
     socket.emit('message', 'Welcome to ChatCord');
+
+    // broadcasting that a user has connected
+    // this will tell everyone that the user has connected except the user itself
+    socket.broadcast.emit('message', 'A user has entered the chat');
+
+    // runs when client disconnects
+    socket.on('disconnect', () => {
+        // if we want to broadcast the message to everyone
+        io.emit('message', 'A user has left the chat');
+    });
+
+    // listen for chat message
+    socket.on('chatMessage', msg =>{
+        io.emit('message', msg);
+    });
 });
 
 const PORT = 3000 || process.env.PORT;
